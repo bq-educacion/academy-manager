d16559d136768ace8a011e66f540fa6929715227
YBlas <make202615@gmail.com>
2022-07-26 11:04:28 +0200

    feature/AC-2 log in page

---
+++ packages/ui/src/components/GoogleButton.tsx
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+import { FC } from "react";
+import { colors } from "../theme";
+import { ImageLogoGoogle } from "@academy-manager/ui";
+
+export const GoogleButton: FC = () => {
+    return(
+        <GButton>
+            <ImageLogoGoogle/>
+        </GButton>
+    )
+}
+
+const GButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 191px;
+    height: 40px;
+    border: solid 1px ${colors.blackBackground};
+    border-radius: 4px;
+    background-color: ${colors.white};
+
+    &:hover {
+        border: solid 1px ${colors.grayBlue};
+    }
+`
