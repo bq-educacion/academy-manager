d600932433fbcda629134aabc895617cebca8c8d
YBlas <make202615@gmail.com>
2022-07-29 09:12:19 +0200

    feature notification icon

--- packages/ui/src/assets/icons/index.ts
+++ packages/ui/src/assets/icons/index.ts
@@ -2,5 +2,6 @@ import ClosePannel from "./close-pannel.svg";
 import Triangle from "./triangle.svg";
 import MenuKebab from "./icon-menukebab.svg";
 import Lens from "./lens.svg";
+import Bell from "./bell.svg";
 
-export { ClosePannel, Triangle, MenuKebab, Lens };
+export { ClosePannel, Triangle, MenuKebab, Lens, Bell };
--- packages/ui/src/components/SecondActionButton.tsx
+++ packages/ui/src/components/SecondActionButton.tsx
@@ -2,12 +2,13 @@ import styled from "@emotion/styled";
 import { FC } from "react";
 import { colors } from "../theme";
 import { P3 } from "../theme/styles";
+import { Bell } from "@academy-manager/ui/src/assets/icons";
 
 
 const SecondActionButton:FC<{disable?:boolean}> =({disable})=>{
     const disabled = disable ? true : false;
     return(
-        <Circle border={true} disable={disabled}><P3User>?</P3User></Circle>
+        <Circle border={true} disable={disabled}><Bell/></Circle>
     )
 }
 
@@ -21,21 +22,25 @@ const Circle = styled.div<{border:boolean, disable:boolean}>`
     margin: 5px;
     background-color: ${(props) => (props.border ? `${colors.colors.white}` : "")};
     border: ${(props) => (props.border ? `1px solid ${colors.colors.grayBlue2}` : "none")};
+
+    & > svg {
+        color: ${colors.colors.grayBlue2};
+    }
     &:hover {
         border: solid 1px ${colors.colors.gray2};
         cursor: pointer;
-        & > p {
+        & > svg {
             color: ${colors.colors.gray2};
         }
     }
     ${(props) => (props.disable ? `
         border: none;
         background-color: ${colors.colors.gray3};
-        & > p {color: ${colors.colors.gray20}}
+        & > svg {color: ${colors.colors.gray20}}
         &:hover {
             border: none;
             background-color: ${colors.colors.gray3};
-            & > p {color: ${colors.colors.gray20}}
+            & > svg {color: ${colors.colors.gray20}}
         }
     }
     ` : "")}
