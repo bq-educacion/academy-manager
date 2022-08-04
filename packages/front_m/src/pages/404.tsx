332811063d7379c5cf562c8364ceb9ba5196ac59
Alberto Valero <alberto.valero@bq.com>
2022-08-04 10:59:57 +0200

    gitignore

--- packages/front_m/src/pages/404.tsx
+++ packages/front_m/src/pages/404.tsx
@@ -1,11 +1,9 @@
-import { Image404 } from "@academy-manager/ui";
 import { NextPage } from "next";
 
 const NotFoundPage: NextPage = () => {
   return (
     <div>
       <h1>404</h1>
-      <img src={Image404.src} />
     </div>
   );
 };
