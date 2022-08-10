1fd1893e2f57119ce3ceec224e20ac9746acbd59
aldaMarMu <alda.martin@bq.com>
2022-08-10 13:55:38 +0200

    minor

--- packages/front_bo/docker-entrypoint.sh
+++ packages/front_bo/docker-entrypoint.sh
@@ -3,4 +3,4 @@ if [ -d /root/config/ ]; then
    cat /root/config/env >> /usr/src/app/packages/front_bo/.env
 fi
 
-/bin/bash -c "pm2 start yarn --interpreter bash --name front-web --no-daemon -- start --wait-ready -p 3000"
+/bin/bash -c "pm2 start yarn start"
