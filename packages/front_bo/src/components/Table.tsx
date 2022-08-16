6b7bc29d8fabf12fcfea87fb5d6b8d70daa98703
Alberto Valero <alberto.valero@bq.com>
2022-08-16 11:15:35 +0200

    manage content

--- packages/front_bo/src/components/Table.tsx
+++ packages/front_bo/src/components/Table.tsx
@@ -9,8 +9,13 @@ type Data = {
 
 type TableProps<T> = {
   data: T[];
-  columns: { label: string; key: string }[];
+  columns: {
+    label: string;
+    key: string;
+    content: (item: T) => React.ReactNode;
+  }[];
   order: { key: string; direction: number };
+
   onSetOrder: (key: string, direction: number) => void;
 };
 
@@ -19,43 +24,22 @@ const Table = <T extends Data>({
   columns,
   order,
   onSetOrder,
-}: TableProps<T>) => {
-  // eslint-disable-next-line
-  const valueByKey = (o: Object, s: string) => {
-    s = s.replace(/\[(\w+)\]/g, ".$1"); // convert indexes to properties
-    s = s.replace(/^\./, ""); // strip a leading dot
-    const key = s.split(".");
-
-    // eslint-disable-next-line
-    return key.reduce((acc, k) => acc[k as keyof Object], o);
-  };
-
-  return (
-    <TableGrid columns={data.length}>
-      {columns.map((column) => (
-        <HeaderCell
-          onClick={() => onSetOrder(column.key, -1 * order.direction)}
-        >
-          {column.label}
-        </HeaderCell>
-      ))}
-      {data.map((item) => (
-        <React.Fragment key={item.id}>
-          {columns.map((column) => (
-            <Cell key={`${item.id}-${column.key}`}>
-              {Array.isArray(valueByKey(item, column.key))
-                ? (valueByKey(item, column.key) as Array<string | number>).join(
-                    ", "
-                  )
-                : valueByKey(item, column.key).toString()}
-            </Cell>
-          ))}
-        </React.Fragment>
-      ))}
-    </TableGrid>
-  );
-};
-
+}: TableProps<T>) => (
+  <TableGrid columns={data.length}>
+    {columns.map((column) => (
+      <HeaderCell onClick={() => onSetOrder(column.key, -1 * order.direction)}>
+        {column.label}
+      </HeaderCell>
+    ))}
+    {data.map((item) => (
+      <React.Fragment key={item.id}>
+        {columns.map((column) => (
+          <Cell key={`${item.id}-${column.key}`}>{column.content(item)}</Cell>
+        ))}
+      </React.Fragment>
+    ))}
+  </TableGrid>
+);
 const TableGrid = styled.div<{ columns: number }>`
   display: grid;
   grid-template-columns: repeat(columns, auto);
