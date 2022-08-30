44dc9e13345e261cbcd130cbe64cc96a4e657e65
Paula Ruiz <paula.ruiz@bq.com>
2022-08-30 09:31:28 +0200

    bugfix/AC-153

--- packages/api_bo/src/lib/validHour.ts
+++ packages/api_bo/src/lib/validHour.ts
@@ -4,20 +4,9 @@ export const validHour = (
   hours: Timetable[],
 ): void => {
   hours.forEach((hour) => {
-    const [start_h, start_m] = hour.start.split(":");
-    const [end_h, end_m] = hour.end.split(":");
-    if (!start_h || !start_m || !end_h || !end_m) {
-      throw new Error("400, Invalid hour");
-    }
     if (
-      Number(start_h) < 0 || Number(start_h) > 23 || Number(start_m) < 0 ||
-      Number(start_m) > 59
-    ) {
-      throw new Error("400, Invalid hour");
-    }
-    if (
-      Number(end_h) < 0 || Number(end_h) > 23 || Number(end_m) < 0 ||
-      Number(end_m) > 59
+      hour.start !== `^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$` ||
+      hour.end !== `^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$`
     ) {
       throw new Error("400, Invalid hour");
     }
