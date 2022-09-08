41705e2b0efd412e3034b74939e2e7c7d98d5342
Paula Ruiz <paula.ruiz@bq.com>
2022-09-08 17:44:51 +0200

    update methods

--- packages/api_bo/src/resolvers/areas.ts
+++ packages/api_bo/src/resolvers/areas.ts
@@ -1,89 +1,79 @@
-//import { areaCollection, AreaModel } from "../models/AreaModel.ts";
+import { Context } from "../app.ts";
+import { ObjectId } from "objectId";
+import { areaCollection, AreaModel } from "../models/AreaModel.ts";
+import {
+  MutationCreateAreaArgs,
+  MutationDeleteAreaArgs,
+  QueryGetAreaArgs,
+  QueryGetAreasArgs,
+} from "../types.ts";
 
 export const areas = {
-  // Area: {
-  //   id: (parent: AreaModel): string => {
-  //     return String(parent._id!);
-  //   }
-  // },
-  // Query: {
-  //   getAreas: async (
-  //     _parent: unknown,
-  //     args: QueryGetAreasArgs,
-  //     ctx: Context,
-  //   ): Promise<AreaModel[]> => {
-  //     try{
-  //         return await areaCollection(ctx.db).find({ region: args.region }).toArray();
-  //     }catch(error){
-  //         throw new Error("500, " + error);
-  //     }
-  //   },
+  Area: {
+    id: (parent: AreaModel): string => {
+      return String(parent._id!);
+    },
+  },
+  Query: {
+    getAreas: async (
+      _parent: unknown,
+      args: QueryGetAreasArgs,
+      ctx: Context,
+    ): Promise<AreaModel[]> => {
+      try {
+        return await areaCollection(ctx.db).find({ region: args.region })
+          .toArray();
+      } catch (error) {
+        throw new Error("500, " + error);
+      }
+    },
 
-  //   getArea: async (
-  //     _parent: unknown,
-  //     args: QueryGetAreaArgs,
-  //     ctx: Context,
-  //   ): Promise<AreaModel> => {
-  //     try {
-  //       const area = await areaCollection(ctx.db).findById(args.id);
-  //       if (!area) {
-  //         throw new Error("400, Area not found");
-  //       }
-  //       return area;
-  //     } catch (error) {
-  //       throw new Error("500, " + error);
-  //     }
-  //   },
-  // },
-  // Mutation: {
-  //   createArea:  (
-  //     _parent: unknown,
-  //     args: MutationCreateAreaArgs,
-  //     ctx: Context,
-  //   ): Promise<GroupModel> => {
-  //     try {
+    getArea: async (
+      _parent: unknown,
+      args: QueryGetAreaArgs,
+      ctx: Context,
+    ): Promise<AreaModel> => {
+      try {
+        const area = await areaCollection(ctx.db).findById(args.id);
+        if (!area) {
+          throw new Error("400, Area not found");
+        }
+        return area;
+      } catch (error) {
+        throw new Error("500, " + error);
+      }
+    },
+  },
+  Mutation: {
+    createArea: async (
+      _parent: unknown,
+      args: MutationCreateAreaArgs,
+      ctx: Context,
+    ): Promise<AreaModel> => {
+      try {
+        const idArea = await areaCollection(ctx.db).insertOne({ ...args });
+        return {
+          _id: idArea,
+          ...args,
+        };
+      } catch (error) {
+        throw new Error("500, " + error);
+      }
+    },
 
-  //     } catch (error) {
-  //       throw new Error("500, " + error);
-  //     }
-  //   },
-
-  //   deleteGroup: async (
-  //     _parent: unknown,
-  //     args: MutationDeleteGroupArgs,
-  //     ctx: Context,
-  //   ): Promise<GroupModel> => {
-  //     try {
-  //       const deletedGroup = await groupCollection(ctx.db).findAndModify(
-  //         { _id: new ObjectId(args.id) },
-  //         {
-  //           remove: true,
-  //         },
-  //       );
-  //       if (!deletedGroup) {
-  //         throw new Error("404, Group not found");
-  //       }
-
-  //       // if students are not in other groups, activeGroup = false
-  //       setActiveToFalse(
-  //         deletedGroup.students,
-  //         groupCollection(ctx.db),
-  //         "students",
-  //         studentCollection(ctx.db),
-  //       );
-
-  //       // if instructors are not in other groups, activeGroup = false
-  //       setActiveToFalse(
-  //         deletedGroup.instructors,
-  //         groupCollection(ctx.db),
-  //         "instructors",
-  //         instructorCollection(ctx.db),
-  //       );
-
-  //       return deletedGroup;
-  //     } catch (error) {
-  //       throw new Error("500, " + error);
-  //     }
-  //   },
-  // },
+    deleteArea: async (
+      _parent: unknown,
+      args: MutationDeleteAreaArgs,
+      ctx: Context,
+    ): Promise<AreaModel> => {
+      try {
+        return await areaCollection(ctx.db).findAndModify(
+          { _id: new ObjectId(args.id) },
+          { remove: true },
+        ) as AreaModel;
+      } catch (error) {
+        throw new Error("500, " + error);
+      }
+    },
+  },
 };
