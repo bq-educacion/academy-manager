9bf5da44221e13b6e1c11f9414205e4683e8db2c
Paula Ruiz <paula.ruiz@bq.com>
2022-09-14 17:19:33 +0200

    update

--- packages/api_bo/src/resolvers/groups.ts
+++ packages/api_bo/src/resolvers/groups.ts
@@ -263,6 +263,7 @@ export const groups = {
     ): Promise<GroupModel> => {
       try {
         checkNotNull(args);
+        let activeGroups = false;
         let updateGroup = { ...args } as Partial<GroupModel>;
 
         if (args.instructors) {
@@ -279,13 +280,22 @@ export const groups = {
           if (exists?.length !== instructors?.length) {
             throw new Error("404, Instructors not found");
           }
+
           updateGroup = { ...updateGroup, instructors };
+
+          if (exists.length > 0) {
+            //check if at least one instructor is enrolled
+            activeGroups = exists.some((instructor) => instructor.enrolled);
+          }
         }
 
         if (args.center) {
-          const exists = await centerCollection(ctx.db).findById(args.center);
+          const exists = await centerCollection(ctx.db).findOne({
+            _id: new ObjectId(args.center),
+            active: true,
+          });
           if (!exists) {
-            throw new Error("404, Center not found");
+            throw new Error("404, Center not found or not active");
           }
           updateGroup = { ...updateGroup, center: new ObjectId(args.center) };
         }
@@ -295,6 +305,8 @@ export const groups = {
           validHour(timetable);
           updateGroup = { ...updateGroup, timetable };
         }
+
+        updateGroup = { ...updateGroup, active: activeGroups };
 
         const newGroup = await groupCollection(ctx.db).findAndModify(
           { _id: new ObjectId(args.id) },
