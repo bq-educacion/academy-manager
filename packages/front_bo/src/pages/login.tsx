bef941dc60d81e7d62bdacf6a0808d7914c295b3
Paula Ruiz <paula.ruiz@bq.com>
2022-09-16 08:57:06 +0200

    begin login front

--- packages/front_bo/src/pages/login.tsx
+++ packages/front_bo/src/pages/login.tsx
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled";
+import { useRouter } from "next/router";
 import { NextPage } from "next";
+// import { v1} from "uuid";
+// import queryString from "query-string";
 import {
   BQLogoGray,
   colors,
@@ -8,9 +11,42 @@ import {
   useTranslate,
 } from "@academy-manager/ui";
 import withApollo from "../apollo/withApollo";
+import { googleAuthEndpoint, googleScopes } from "../config";
 
-const LogInPage: NextPage = () => {
+// const uuid = v1;
+
+const appID = process.env.GOOGLE_CLIENT_ID;
+
+export interface IloginProps {
+  clientId?: string;
+  redirectUri?: string;
+}
+
+const LogInPage: NextPage<IloginProps> = ({
+  clientId = appID,
+  redirectUri = "/google-redirect",
+}) => {
   const t = useTranslate();
+
+  const router = useRouter();
+
+  const onClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    // sessionStorage.setItem("googleAuthState", uuid());
+    // sessionStorage.setItem("googleAuthNonce", uuid());
+    sessionStorage.setItem("googlePrevPathname", router.pathname);
+
+    const location = window.location;
+    const authParams = {
+      response_type: "token",
+      client_id: clientId,
+      redirect_uri: `${location.protocol}//${location.host}/${redirectUri}`,
+      scope: googleScopes,
+    };
+    //queryString.stringify(authParams)
+    location.assign(googleAuthEndpoint + "?" + authParams);
+  };
 
   return (
     <Layout>
@@ -20,7 +56,7 @@ const LogInPage: NextPage = () => {
         <styles.RainbowDivider />
         <LogIn>
           <styles.P4>Accede con tu cuenta BQ</styles.P4>
-          <GoogleButton />
+          <GoogleButton onClick={onClick} />
         </LogIn>
       </LogInBox>
     </Layout>
